8b9976bd08480d0141779076a9429cdf48ef1e23
keFei <z928385924@163.com>
2020-03-08 10:50:56 +0800

    init

---
+++ lib/main_demo1.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// 自定义组件
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        '你好Flutter111',
+        textDirection: TextDirection.ltr,
+
+        style: TextStyle(
+          fontSize: 40.0,
+          color: Colors.yellow,
+        ),
+      ),
+    );
+  }
+
+}
